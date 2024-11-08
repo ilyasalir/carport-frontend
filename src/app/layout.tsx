4d33d202054479e8a3b6c9397f6adfe1fed4dd45
Ilyas Ali Rahman <ilyas14022@gmail.com>
2024-11-08 17:10:57 +0700

    layout default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,30 +44,9 @@ export default function RootLayout({
           name="google-site-verification"
           content="kN_UUw1Kgr3Hfd9zNpmi4MzaVLIHZkyxjJual_vpMTQ"
         />
-        {/* Google Tag Manager (Script) */}
-        <Script strategy="afterInteractive" id="google-tag-manager">
-          {`(function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5S5RPCNS');`}
-        </Script>
       </head>
+      <GoogleTagManager gtmId="AW-956707115" />
       <body className={`${poppins.variable} ${robotoSlab.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5S5RPCNS"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
-        
         <ToastContainer />
         <RegisterProvider>
           <DetailsProvider>
